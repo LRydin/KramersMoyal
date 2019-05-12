113e16efc7e56fa39fec313925f8dcdedd470ef7
Rydin <leonardo.rydin@gmail.com>
2019-05-12 11:07:51 +0200

    Added function to produce a 2d uniform grid, and a grid function

--- kramersmoyal/kernels.py
+++ kramersmoyal/kernels.py
@@ -75,6 +75,32 @@ def Space(n_points,bandwidth):
 	space = space + (space[1] - space[0])
 	return space
 
+def Grid(space, bounds):
+	"""
+	Generates underlying space
+
+	Parameters
+	----------
+	n_points  : integer
+		Number ( >= 1) total array size of the kernel. Must be either smaller
+		than the total size of the phase space, for bounded compact support
+		kernels, or equivalent of the size of the phase space.
+
+	bandwidth  : float
+		Number ( >= 0) indicating the bandwidth of the kernel.
+	Returns
+	-------
+	space  : array
+		The underlying space
+	"""
+	# For a bandwidth of 1, the kernel size is one tenth of the phase space
+	kernel_size = int(n_points * bandwidth / 10)
+	# linear space
+	space = np.linspace(-1 * bandwidth, 1 * bandwidth, kernel_size,
+	 					endpoint=False)
+	space = space + (space[1] - space[0])
+	return space
+
 # 1 Dimensional Kernels
 
 def Epanechnikov_1d(n_points, bandwidth='optimal'):
@@ -287,6 +313,67 @@ def Epanechnikov_2d(n_points, bandwidth = 'optimal', data=False, bounds=np.array
 
 	if symmetric == True:
 		# Epanechnikov kernel:
+		#   (8/3*pi)*3/4(1 - (x² + y²), x=-1 to x=1 CHECK!
+		kernel = 1 - (np.power(x1_2D,2) + np.power(y1_2D,2)) / (np.power(bandwidth,2))
+		#Remove <0 values
+		kernel[kernel < 0.] = 0.0
+		normalisation = 2 / (bandwidth * np.pi)
+
+	elif symmetric == False:
+		# Epanechnikov kernel:
+		#   (8/3*pi)*3/4(1 - (x + y)², x=-1 to x=1 CHECK!
+		kernel = (1 - (np.power(x1_2D,2)))*(1 - (np.power(y1_2D,2))) / (np.power(bandwidth,2))
+		#Remove <0 values
+		kernel[kernel < 0.] = 0.0
+		normalisation = 2 / (bandwidth * np.pi)
+
+	return kernel * normalisation
+
+def Quartic_2d(n_points, bandwidth = 'optimal', data=False, bounds=np.array([]),
+					symmetric=True):
+	"""
+	Generates the symmetric Epanechnikov kernel in 2 dimension
+
+	Parameters
+	----------
+	n_points  : integer
+		Number ( >= 1) total array size of the whole phase space. The kernel
+		will be of a reduced size in this 2D space of size n_points x n_point.
+		Suggested values is of order n_points = 1000.
+
+	bandwidth  : float
+		Number ( >= 0) indicating the bandwidth of the kernel. If unspecified,
+		the optimal bandwidth according to Silverman will be used.
+		Can require the argument  : data
+
+	data  : array (2D)
+	   Data where to apply the kernel. Needed to calculate the extrema, if not
+	   given by argument bounds, or to	calculate the optimal bandwidth.
+
+	bounds  : array
+	   Either an array of two entries, mininum and maximum, or an 2D array of
+	   minimum and maximum in each dimension. If unspecified and argument data
+	   is passed as argument, extrema will be taken from there.
+
+	symmetric  : boolean
+		The Epanechnikov kernel in two dimensional has a symmetric and a
+		non-symmetric version.
+
+	Returns
+	-------
+	kernel  : array (float)
+		The specified kernel.
+	"""
+
+	#Produce underlying space
+	space = Space(n_points,bandwidth)
+	# 2d linear space for Kernel generator
+	x1_2D, y1_2D = np.meshgrid(space, space, sparse=True)                      # 2d linear space for Kernel generator
+
+
+
+	if symmetric == True:
+		# Quartic, or biweight, kernel in 2d:
 		#   (8/3*pi)*3/4(1 - (x² + y²), x=-1 to x=1 CHECK!
 		kernel = 1 - (np.power(x1_2D,2) + np.power(y1_2D,2)) / (np.power(bandwidth,2))
 		#Remove <0 values
