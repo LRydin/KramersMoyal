ba777bf646f70260a0964c63d7cfb3e8cec641c9
Francisco Meirinhos <fcmeirinhos@gmail.com>
2019-07-05 13:46:43 +0200

    Added binning test

---
+++ test/binning_test.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+import sys
+sys.path.append("../")
+from kramersmoyal.binning import histogramdd
+
+N = 1_000_000
+dim = 2
+bins = np.array([100] * 2)
+timeseries = np.random.rand(N, dim)
+
+Nw = 10
+weights = np.random.rand(N, Nw)
+
+hist1 = list()
+for w in weights.T:
+    hist1.append(np.histogramdd(timeseries, bins=bins, weights=w)[0])
+
+hist2 = histogramdd(timeseries, bins=bins, weights=weights)[0]
+
+assert np.array(
+    list(map(lambda i: (hist1[i] == hist2[..., i]), range(Nw)))).any()
