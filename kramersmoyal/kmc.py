6aa8dbaa2f97152c8a035e096ab2daa8b3a0e5ea
Francisco Meirinhos <fcmeirinhos@gmail.com>
2019-07-07 13:39:35 +0200

    Removed tester, added example and improved cartesian product

--- kramersmoyal/kmc.py
+++ kramersmoyal/kmc.py
@@ -1,32 +1,40 @@
 import numpy as np
 from scipy.signal import convolve
-from itertools import product
-
 from .binning import histogramdd
 
 
-def kmc_kernel_estimator(timeseries: np.ndarray, kernel: callable, bw=bw,
-                         bins: np.ndarray, powers: np.ndarray):
+def kmc_kernel_estimator(timeseries: np.ndarray, bins: np.ndarray,
+                         kernel: callable, bw: float,
+                         powers: np.ndarray):
     """
     Estimates Kramers-Moyal coefficients from a timeseries using a kernel
     estimator method.
     """
+
+    def cartesian_product(arrays: np.ndarray):
+        # Taken from https://stackoverflow.com/questions/11144513
+        la = len(arrays)
+        arr = np.empty([len(a) for a in arrays] + [la], dtype=np.float64)
+        for i, a in enumerate(np.ix_(*arrays)):
+            arr[..., i] = a
+        return arr.reshape(-1, la)
+
     # Calculate derivatives and its powers
-    grads = np.diff(timeseries)
+    grads = np.diff(timeseries, axis=0)
     weights = np.prod(np.power(grads[..., None], powers), axis=1)
 
     # Get weighted histogram
-    hist, edges = histogramdd(timeseries, bins=bins,
+    hist, edges = histogramdd(timeseries[1:, ...], bins=bins,
                               weights=weights, density=True)
 
     # Generate kernel
-    mesh = np.asarray(list(product(*edges)))
+    mesh = cartesian_product(edges)
     kernel_ = kernel(mesh, bw=bw).reshape(*(edge.size for edge in edges))
     kernel_ /= np.sum(kernel_)
 
     kmc = list()
     # Convolve with kernel for all powers
     for p in range(powers.shape[1]):
-        kmc.append(convolve(hist[..., p], kernel_, mode='full'))
+        kmc.append(convolve(kernel_, hist[..., p], mode='same'))
 
-    return np.stack(kmc, axis=-1)
+    return np.stack(kmc, axis=-1), edges
