f66c9946c052df83a72ba84c41396962b714f323
LRydin <leonardo.rydin@gmail.com>
2020-01-01 18:52:46 +0100

    adding functions to tests for pytest to autodetect

--- test/binning_test.py
+++ test/binning_test.py
@@ -1,24 +1,25 @@
 import numpy as np
 
-import sys
-sys.path.append("../")
+# import sys
+# sys.path.append("../")
 from kramersmoyal.binning import histogramdd
 
 N = 1_000_000
 
-for dim in [1, 2]:
-    bins = np.array([30] * dim)
-    timeseries = np.random.rand(N, dim)
+def test_binning():
+    for dim in [1, 2]:
+        bins = np.array([30] * dim)
+        timeseries = np.random.rand(N, dim)
 
-    Nw = 10
-    weights = np.random.rand(N, Nw)
+        Nw = 10
+        weights = np.random.rand(N, Nw)
 
-    hist1 = [np.histogramdd(timeseries, bins=bins,
-                            weights=w, density=True)[0] for w in weights.T]
+        hist1 = [np.histogramdd(timeseries, bins=bins,
+                                weights=w, density=True)[0] for w in weights.T]
 
-    hist2 = histogramdd(timeseries, bins=bins,
-                        weights=weights.T, density=True)[0]
+        hist2 = histogramdd(timeseries, bins=bins,
+                            weights=weights.T, density=True)[0]
 
-    assert np.array(
-        list(map(lambda i: (hist1[i] == hist2[i, ...]), range(Nw)))).all()
-    print("Binning in {0}D: passed".format(dim))
+        assert np.array(
+            list(map(lambda i: (hist1[i] == hist2[i, ...]), range(Nw)))).all()
+        print("Binning in {0}D: passed".format(dim))
--- test/kernels_test.py
+++ test/kernels_test.py
@@ -1,19 +1,20 @@
 import numpy as np
 from itertools import product
 
-import sys
-sys.path.append("..")
+# import sys
+# sys.path.append("..")
 from kramersmoyal.kernels import *
 
-for dim in [1, 2, 3]:
-    edges = [np.linspace(-10, 10, 100000 / 10**dim, endpoint=True)] * dim
-    mesh = np.asarray(list(product(*edges)))
-    dx = (edges[0][1] - edges[0][0]) ** dim
-    for kernel in [epanechnikov, gaussian, uniform]:
-        for bw in [0.1, 0.3, 0.5, 1.0, 1.5, 2.0]:
-            kernel_ = kernel(mesh, bw=bw).reshape(
-                *(edge.size for edge in edges))
-            passed = np.allclose(kernel_.sum() * dx, 1, atol=1e-3)
-            print("Kernel {0:10s}\t with {1:.2f} bandwidth at {2}D passed: {3}".format(
-                kernel.__name__, bw, dim, passed))
-        print()
+def test_kernels():
+    for dim in [1, 2, 3]:
+        edges = [np.linspace(-10, 10, 100000 / 10**dim, endpoint=True)] * dim
+        mesh = np.asarray(list(product(*edges)))
+        dx = (edges[0][1] - edges[0][0]) ** dim
+        for kernel in [epanechnikov, gaussian, uniform]:
+            for bw in [0.1, 0.3, 0.5, 1.0, 1.5, 2.0]:
+                kernel_ = kernel(mesh, bw=bw).reshape(
+                    *(edge.size for edge in edges))
+                passed = np.allclose(kernel_.sum() * dx, 1, atol=1e-3)
+                print("Kernel {0:10s}\t with {1:.2f} bandwidth at {2}D passed: {3}".format(
+                    kernel.__name__, bw, dim, passed))
+            print()
