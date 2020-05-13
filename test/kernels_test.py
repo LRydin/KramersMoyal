619d8c5d6612cf5c0a5aa4965143e66bccb3d1e9
LRydin <leonardo.rydin@gmail.com>
2020-05-13 18:39:47 +0200

    adding triagular to test of kernels

--- test/kernels_test.py
+++ test/kernels_test.py
@@ -7,10 +7,10 @@ from kramersmoyal.kernels import *
 
 def test_kernels():
     for dim in [1, 2, 3]:
-        edges = [np.linspace(-10, 10, 100000 / 10**dim, endpoint=True)] * dim
+        edges = [np.linspace(-10, 10, 100000 // 10**dim, endpoint=True)] * dim
         mesh = np.asarray(list(product(*edges)))
         dx = (edges[0][1] - edges[0][0]) ** dim
-        for kernel in [epanechnikov, gaussian, uniform]:
+        for kernel in [epanechnikov, gaussian, uniform, triagular]:
             for bw in [0.1, 0.3, 0.5, 1.0, 1.5, 2.0]:
                 kernel_ = kernel(mesh, bw=bw).reshape(
                     *(edge.size for edge in edges))
@@ -18,3 +18,6 @@ def test_kernels():
                 print("Kernel {0:10s}\t with {1:.2f} bandwidth at {2}D passed: {3}".format(
                     kernel.__name__, bw, dim, passed))
             print()
+
+
+test_kernels()
